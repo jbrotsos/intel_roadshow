9bc321eec8fd3c2eb5ab859ed4e36c4f3a1f9f7b
Vincent Rubiolo <vincent.libre@cryostase.eu>
2015-06-05 00:22:43 -0700

    Chassis: added simple directional control
    Able to go forward/backward/turn right/turn left.
    
    Everything is done at constant speed.

--- roadshow/chassis/chassis2motor-pololu-shield/chassis2motor-pololu-shield.ino
+++ roadshow/chassis/chassis2motor-pololu-shield/chassis2motor-pololu-shield.ino
@@ -1,7 +1,7 @@
 #include <DRV8835MotorShield.h>
 
-
 #define LED_PIN 13
+#define SPEED 200
 
 DRV8835MotorShield motors;
 
@@ -11,74 +11,52 @@ void setup()
   
   // uncomment if direction need to be flipped
   //motors.flipM1(true);
-  //motors.flipM2(true);
+  motors.flipM2(true);
+}
+
+void move (int speed, int secs, bool forwardLeft, bool forwardRight)
+{
+  int lspeed = speed;
+  int rspeed = speed;
+  
+  if (!forwardLeft)
+  {
+  lspeed = -lspeed;
+  }
+  if (!forwardRight)
+  {
+  rspeed = -rspeed;
+  }
+  
+motors.setSpeeds (lspeed, rspeed);
+delay (secs * 1000);
+motors.setSpeeds (0, 0);
+}
+
+void forward (int speed, int secs)
+{
+  move (speed, secs, true, true);
+}
+
+void backwards (int speed, int secs)
+{
+  move (speed, secs, false, false);
+}
+
+void turn_right ()
+{
+  move (SPEED, 3, true, false);
+}
+
+void turn_left ()
+{
+  move (SPEED, 3, false, true);
 }
 
 void loop()
 {
-  // run M1 motor with positive speed
-  
-  digitalWrite(LED_PIN, HIGH);
-  
-  for (int speed = 0; speed <= 400; speed++)
-  {
-    motors.setM1Speed(speed);
-    delay(2);
-  }
-
-  for (int speed = 400; speed >= 0; speed--)
-  {
-    motors.setM1Speed(speed);
-    delay(2);
-  }
-  
-  // run M1 motor with negative speed
-  
-  digitalWrite(LED_PIN, LOW);
-  
-  for (int speed = 0; speed >= -400; speed--)
-  {
-    motors.setM1Speed(speed);
-    delay(2);
-  }
-  
-  for (int speed = -400; speed <= 0; speed++)
-  {
-    motors.setM1Speed(speed);
-    delay(2);
-  }
-
-  // run M2 motor with positive speed
-  
-  digitalWrite(LED_PIN, HIGH);
-  
-  for (int speed = 0; speed <= 400; speed++)
-  {
-    motors.setM2Speed(speed);
-    delay(2);
-  }
-
-  for (int speed = 400; speed >= 0; speed--)
-  {
-    motors.setM2Speed(speed);
-    delay(2);
-  }
-  
-  // run M2 motor with negative speed
-  
-  digitalWrite(LED_PIN, LOW);
-  
-  for (int speed = 0; speed >= -400; speed--)
-  {
-    motors.setM2Speed(speed);
-    delay(2);
-  }
-  
-  for (int speed = -400; speed <= 0; speed++)
-  {
-    motors.setM2Speed(speed);
-    delay(2);
-  }
-  
-  delay(500);
+  forward (SPEED, 5);
+  backwards (SPEED, 5);
+  turn_right();
+  turn_left();
 }
